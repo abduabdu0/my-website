0d53b13e5ff85f9ef838cf77cdbe5151b8da462e
abduabdu <187637592+abduabdu0@users.noreply.github.com>
2026-04-02 22:28:25 +0500

    Add files via upload

---
+++ mir/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("love-app").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "icon.png",
+        "photo1.jpg",
+        "photo2.jpg",
+        "music.mp3"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        navigator.vibrate && navigator.vibrate(50);
+    });
+});
